9133357ed806b02ee69cb1a13911473ac9f6e2f7
alchemycodess <heyiamfatima1@gmail.com>
2025-12-09 23:44:30 +0530

    chore: organize files
    Signed-off-by: alchemycodess <heyiamfatima1@gmail.com>

--- recipe-fe/src/App.tsx
+++ recipe-fe/src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { Subscribe } from "./components/Subscribe";
 
 function App() {
-  return <div className="bg-blue-600">hello world</div>;
+  return (
+    <div>
+      <Navbar />
+      <Dashboard />
+      <Subscribe />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
